00bfd5c7449c0e35a46c5cd6ed1b37c72cb3dd7d
kewenle <chunzhi.hu@allride.ai>
2019-05-09 20:45:20 +0800

    add one function,the second commit

--- python_test.py
+++ python_test.py
@@ -1,6 +1,10 @@
 def print_hello():
     print('hello world')
 
+def print_hello_new(who):
+    print('hello ' + who)
 
 if __name__=='__main__':
+    name = 'china'
     print_hello()
+    print_hello_new(name)
